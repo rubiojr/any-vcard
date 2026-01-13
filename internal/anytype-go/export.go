6a3e3b579d0eea8bc5de868937422ec277218845
Sergio Rubio <rubiojr@rbel.co>
2026-01-13 18:04:24 +0100

    remove anytype-go vendored code

--- internal/anytype-go/export.go
+++
@@ -1,7 +0,0 @@
-package anytype
-
-// ExportResult represents the result of an object export operation
-type ExportResult struct {
-	Markdown string `json:"markdown,omitempty"`
-	// Add other export format fields as they become available
-}
